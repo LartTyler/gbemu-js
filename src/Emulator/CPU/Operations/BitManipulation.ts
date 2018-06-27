7f444e6ae66dca28bfb20a625966c6ff1eaa7db1
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-06-26 22:55:18 -0400

    Add RR and RRC operators

--- src/Emulator/CPU/Operations/BitManipulation.ts
+++ src/Emulator/CPU/Operations/BitManipulation.ts
@@ -1,5 +1,6 @@
 import {ResetOperators, ResetOperatorSet} from './BitManipulation/Reset';
 import {RotateLeftOperators, RotateLeftOperatorSet} from './BitManipulation/RotateLeft';
+import {RotateRightOperators, RotateRightOperatorSet} from './BitManipulation/RotateRight';
 import {SetOperators, SetOperatorSet} from './BitManipulation/Set';
 import {TestOperators, TestOperatorSet} from './BitManipulation/Test';
 import {OperatorSet} from './index';
@@ -8,12 +9,14 @@ export interface BitManipulationOperatorSet extends OperatorSet,
 	TestOperatorSet,
 	ResetOperatorSet,
 	RotateLeftOperatorSet,
+	RotateRightOperatorSet,
 	SetOperatorSet {
 }
 
 export const BitManipulationOperators: BitManipulationOperatorSet = {
 	...ResetOperators,
 	...RotateLeftOperators,
+	...RotateRightOperators,
 	...TestOperators,
 	...SetOperators,
 };
