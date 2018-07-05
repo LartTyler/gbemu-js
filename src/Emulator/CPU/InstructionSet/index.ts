015c4829dc413920e2c9a25c1e77ba627ec09627
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-07-05 16:33:46 -0400

    Add @see for opcode reference chart

--- src/Emulator/CPU/InstructionSet/index.ts
+++ src/Emulator/CPU/InstructionSet/index.ts
@@ -1,6 +1,8 @@
 import {InstructionManager} from './InstructionManager';
 import {LoadOperators} from './Operators/Load';
 
+// @see http://z80-heaven.wikidot.com/opcode-reference-chart
+
 export const PrimaryInstructions = new InstructionManager([
 	...LoadOperators,
 ]);
