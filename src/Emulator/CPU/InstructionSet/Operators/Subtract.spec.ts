b93236fc14b6a2b972fc7aa13b6e55d1137eb18e
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-07-12 13:55:15 -0400

    WIP

--- src/Emulator/CPU/InstructionSet/Operators/Subtract.spec.ts
+++ src/Emulator/CPU/InstructionSet/Operators/Subtract.spec.ts
@@ -85,7 +85,196 @@ describe('Subtract operators', () => {
 		expect(operator).not.toBeNull();
 		expect(operator.name).toBe('SubtractHLAddress');
 
-		// TODO Finish SubtractHLAddress
+		registers.h = 0xC0;
+		registers.l = 0x00;
+
+		registers.a = 3;
+		memory.writeByte(0xC000, 1);
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(2);
+		expect(registers.m).toBe(2);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION);
+
+		registers.a = 1;
+		memory.writeByte(0xC000, 5);
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(252);
+		expect(registers.m).toBe(2);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.CARRY | RegisterFlag.HALF_CARRY);
+
+		registers.a = 1;
+		memory.writeByte(0xC000, 1);
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(0);
+		expect(registers.m).toBe(2);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.ZERO);
+
+		registers.a = 16;
+		memory.writeByte(0xC000, 3);
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(13);
+		expect(registers.m).toBe(2);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.HALF_CARRY);
+	});
+
+	test('SubtractPCAddress', () => {
+		const operator = PrimaryInstructions.getByCode(0xD6);
+
+		expect(operator).not.toBeNull();
+		expect(operator.name).toBe('SubtractPCAddress');
+
+		registers.programCount = 0xC000;
+
+		registers.a = 3;
+		memory.writeByte(registers.programCount, 1);
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(2);
+		expect(registers.m).toBe(2);
+		expect(registers.programCount).toBe(0xC001);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION);
+
+		registers.a = 1;
+		memory.writeByte(registers.programCount, 5);
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(252);
+		expect(registers.m).toBe(2);
+		expect(registers.programCount).toBe(0xC002);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.CARRY | RegisterFlag.HALF_CARRY);
+
+		registers.a = 1;
+		memory.writeByte(registers.programCount, 1);
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(0);
+		expect(registers.m).toBe(2);
+		expect(registers.programCount).toBe(0xC003);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.ZERO);
+
+		registers.a = 16;
+		memory.writeByte(registers.programCount, 3);
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(13);
+		expect(registers.m).toBe(2);
+		expect(registers.programCount).toBe(0xC004);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.HALF_CARRY);
+	});
+	// endregion
+
+	// region Subtract registers with carry
+	const runRegisterWithCarryTests = (key: RegisterKey, opcode: number): void => {
+		const operator = PrimaryInstructions.getByCode(opcode);
+
+		expect(operator).not.toBeNull();
+		expect(operator.name).toBe(`Subtract${key.toUpperCase()}WithCarry`);
+
+		registers.a = 3;
+		registers[key] = 1;
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(2);
+		expect(registers.m).toBe(1);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION);
+
+		registers.a = 0;
+		registers[key] = 3;
+		registers.flags = 0;
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(253);
+		expect(registers.m).toBe(1);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.CARRY | RegisterFlag.HALF_CARRY);
+
+		registers.a = 1;
+		registers[key] = 1;
+		registers.flags = 0;
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(0);
+		expect(registers.m).toBe(1);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.ZERO);
+
+		registers.a = 18;
+		registers[key] = 5;
+		registers.flags = 0;
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(13);
+		expect(registers.m).toBe(1);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.HALF_CARRY);
+	};
+
+	test('SubtractAWithCarry', () => {
+		const operator = PrimaryInstructions.getByCode(0x9F);
+
+		expect(operator).not.toBeNull();
+		expect(operator.name).toBe(`SubtractAWithCarry`);
+
+		registers.a = 3;
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(0);
+		expect(registers.m).toBe(1);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.ZERO);
+
+		registers.a = 3;
+		registers.flags = RegisterFlag.CARRY;
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(255);
+		expect(registers.m).toBe(1);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION | RegisterFlag.CARRY | RegisterFlag.HALF_CARRY);
+	});
+
+	test('SubtractBWithCarry', () => runRegisterWithCarryTests('b', 0x98));
+	test('SubtractCWithCarry', () => runRegisterWithCarryTests('c', 0x99));
+	test('SubtractDWithCarry', () => runRegisterWithCarryTests('d', 0x9A));
+	test('SubtractEWithCarry', () => runRegisterWithCarryTests('e', 0x9B));
+	test('SubtractHWithCarry', () => runRegisterWithCarryTests('h', 0x9C));
+	test('SubtractLWithCarry', () => runRegisterWithCarryTests('l', 0x9D));
+	// endregion
+
+	// region Subtract address with carry
+	test('SubtractHLAddressWithCarry', () => {
+		const operator = PrimaryInstructions.getByCode(0x9E);
+
+		expect(operator).not.toBeNull();
+		expect(operator.name).toBe('SubtractHLAddressWithCarry');
+
+		registers.h = 0xC0;
+		registers.l = 0x00;
+
+		registers.a = 3;
+		memory.writeByte(0xC000, 1);
+
+		operator.invoke(hardware);
+
+		expect(registers.a).toBe(2);
+		expect(registers.m).toBe(2);
+		expect(registers.flags).toBe(RegisterFlag.OPERATION);
+
+		registers.a = 0;
+		memory.writeByte(0xC000, 3);
 	});
 	// endregion
 });
--- src/Emulator/CPU/InstructionSet/Operators/Subtract.ts
+++ src/Emulator/CPU/InstructionSet/Operators/Subtract.ts
@@ -2,14 +2,12 @@ import {HardwareBusInterface} from '../../../Hardware';
 import {RegisterFlag, RegisterKey} from '../../Registers';
 import {Operator, OperatorInterface} from '../InstructionManager';
 
-const subtract = (key: RegisterKey, hardware: HardwareBusInterface): void => {
+const subtract = (value: number, hardware: HardwareBusInterface): void => {
 	const registers = hardware.registers;
-
-	const originalA = registers.a;
-	const originalOther = registers[key];
+	const original = registers.a;
 
 	registers.flags = RegisterFlag.OPERATION;
-	registers.a -= originalOther;
+	registers.a -= value;
 
 	if (registers.a < 0)
 		registers.flags |= RegisterFlag.CARRY;
@@ -19,20 +17,84 @@ const subtract = (key: RegisterKey, hardware: HardwareBusInterface): void => {
 	if (!registers.a)
 		registers.flags |= RegisterFlag.ZERO;
 
-	if ((registers.a ^ originalA ^ originalOther) & 0x10)
+	if ((registers.a ^ original ^ value) & 0x10)
 		registers.flags |= RegisterFlag.HALF_CARRY;
+};
+
+const subtractRegister = (key: RegisterKey, hardware: HardwareBusInterface): void => {
+	const registers = hardware.registers;
+
+	subtract(registers[key], hardware);
+
+	registers.m = 1;
+};
+
+const subtractAddress = (address: number, hardware: HardwareBusInterface): void => {
+	const registers = hardware.registers;
+
+	subtract(hardware.memory.readByte(address), hardware);
+
+	registers.m = 2;
+};
+
+const subtractWithCarry = (value: number, hardware: HardwareBusInterface): void => {
+	const registers = hardware.registers;
+	const original = registers.a;
+
+	registers.a -= value;
+
+	if (registers.flags & RegisterFlag.CARRY)
+		registers.a -= 1;
+
+	registers.flags = RegisterFlag.OPERATION;
+
+	if (registers.a < 0)
+		registers.flags |= RegisterFlag.CARRY;
+
+	registers.a &= 255;
+
+	if (!registers.a)
+		registers.flags |= RegisterFlag.ZERO;
+
+	if ((registers.a ^ original ^ value) & 0x10)
+		registers.flags |= RegisterFlag.HALF_CARRY;
+};
+
+const subtractRegisterWithCarry = (key: RegisterKey, hardware: HardwareBusInterface): void => {
+	const registers = hardware.registers;
+
+	subtractWithCarry(registers[key], hardware);
 
 	registers.m = 1;
 };
 
 export const SubtractOperators: OperatorInterface[] = [
 	// region Subtract registers
-	new Operator('SubtractA', 0x97, hardware => subtract('a', hardware)),
-	new Operator('SubtractB', 0x90, hardware => subtract('b', hardware)),
-	new Operator('SubtractC', 0x91, hardware => subtract('c', hardware)),
-	new Operator('SubtractD', 0x92, hardware => subtract('d', hardware)),
-	new Operator('SubtractE', 0x93, hardware => subtract('e', hardware)),
-	new Operator('SubtractH', 0x94, hardware => subtract('h', hardware)),
-	new Operator('SubtractL', 0x95, hardware => subtract('l', hardware)),
+	new Operator('SubtractA', 0x97, hardware => subtractRegister('a', hardware)),
+	new Operator('SubtractB', 0x90, hardware => subtractRegister('b', hardware)),
+	new Operator('SubtractC', 0x91, hardware => subtractRegister('c', hardware)),
+	new Operator('SubtractD', 0x92, hardware => subtractRegister('d', hardware)),
+	new Operator('SubtractE', 0x93, hardware => subtractRegister('e', hardware)),
+	new Operator('SubtractH', 0x94, hardware => subtractRegister('h', hardware)),
+	new Operator('SubtractL', 0x95, hardware => subtractRegister('l', hardware)),
+	// endregion
+
+	// region Subtract address
+	new Operator('SubtractHLAddress', 0x96, hardware => {
+		const registers = hardware.registers;
+
+		subtractAddress((registers.h << 8) + registers.l, hardware);
+	}),
+	new Operator('SubtractPCAddress', 0xD6, hardware => subtractAddress(hardware.registers.programCount++, hardware)),
+	// endregion
+
+	// region Subtract register with carry
+	new Operator('SubtractAWithCarry', 0x9F, hardware => subtractRegisterWithCarry('a', hardware)),
+	new Operator('SubtractBWithCarry', 0x98, hardware => subtractRegisterWithCarry('b', hardware)),
+	new Operator('SubtractCWithCarry', 0x99, hardware => subtractRegisterWithCarry('c', hardware)),
+	new Operator('SubtractDWithCarry', 0x9A, hardware => subtractRegisterWithCarry('d', hardware)),
+	new Operator('SubtractEWithCarry', 0x9B, hardware => subtractRegisterWithCarry('e', hardware)),
+	new Operator('SubtractHWithCarry', 0x9C, hardware => subtractRegisterWithCarry('h', hardware)),
+	new Operator('SubtractLWithCarry', 0x9D, hardware => subtractRegisterWithCarry('l', hardware)),
 	// endregion
 ];
