84d0d7c63ab452862a96c533e1011935a4652c5d
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-07-22 10:08:34 -0400

    Finish bit reset opcodes

--- src/Emulator/CPU/InstructionSet/Operators/BitManipulation/Reset.spec.ts
+++ src/Emulator/CPU/InstructionSet/Operators/BitManipulation/Reset.spec.ts
@@ -11,6 +11,20 @@ describe('Bit reset operators', () => {
 	const hardware = new HardwareBus(new Cpu(), new Memory(), new Gpu(null));
 	const {memory, registers} = hardware;
 
+	beforeEach(() => {
+		hardware.cpu.reset();
+		memory.reset();
+	});
+
+	const getValue = (position: number): number => {
+		const value = 1 << position;
+
+		if (position > 0)
+			return value + (1 << (position - 1));
+
+		return value + 2;
+	};
+
 	// region Registers
 	const runRegisterTests = (key: RegisterKey, position: number, opcode: number): void => {
 		const operator = BitInstructions.getByCode(opcode);
@@ -24,6 +38,79 @@ describe('Bit reset operators', () => {
 
 		expect(registers[key]).toBe(0);
 		expect(registers.m).toBe(2);
+
+		const value = getValue(position);
+
+		registers[key] = value;
+
+		operator.invoke(hardware);
+
+		expect(registers[key]).toBe(value & ~(1 << position));
+		expect(registers.m).toBe(2);
 	};
+
+	test('ResetBit0InA', () => runRegisterTests('a', 0, 0x87));
+	test('ResetBit0InB', () => runRegisterTests('b', 0, 0x80));
+	test('ResetBit0InC', () => runRegisterTests('c', 0, 0x81));
+	test('ResetBit0InD', () => runRegisterTests('d', 0, 0x82));
+	test('ResetBit0InE', () => runRegisterTests('e', 0, 0x83));
+	test('ResetBit0InH', () => runRegisterTests('h', 0, 0x84));
+	test('ResetBit0InL', () => runRegisterTests('l', 0, 0x85));
+
+	test('ResetBit1InA', () => runRegisterTests('a', 1, 0x8F));
+	test('ResetBit1InB', () => runRegisterTests('b', 1, 0x88));
+	test('ResetBit1InC', () => runRegisterTests('c', 1, 0x89));
+	test('ResetBit1InD', () => runRegisterTests('d', 1, 0x8A));
+	test('ResetBit1InE', () => runRegisterTests('e', 1, 0x8B));
+	test('ResetBit1InH', () => runRegisterTests('h', 1, 0x8C));
+	test('ResetBit1InL', () => runRegisterTests('l', 1, 0x8D));
+
+	test('ResetBit2InA', () => runRegisterTests('a', 2, 0x97));
+	test('ResetBit2InB', () => runRegisterTests('b', 2, 0x90));
+	test('ResetBit2InC', () => runRegisterTests('c', 2, 0x91));
+	test('ResetBit2InD', () => runRegisterTests('d', 2, 0x92));
+	test('ResetBit2InE', () => runRegisterTests('e', 2, 0x93));
+	test('ResetBit2InH', () => runRegisterTests('h', 2, 0x94));
+	test('ResetBit2InL', () => runRegisterTests('l', 2, 0x95));
+
+	test('ResetBit3InA', () => runRegisterTests('a', 3, 0x9F));
+	test('ResetBit3InB', () => runRegisterTests('b', 3, 0x98));
+	test('ResetBit3InC', () => runRegisterTests('c', 3, 0x99));
+	test('ResetBit3InD', () => runRegisterTests('d', 3, 0x9A));
+	test('ResetBit3InE', () => runRegisterTests('e', 3, 0x9B));
+	test('ResetBit3InH', () => runRegisterTests('h', 3, 0x9C));
+	test('ResetBit3InL', () => runRegisterTests('l', 3, 0x9D));
+
+	test('ResetBit4InA', () => runRegisterTests('a', 4, 0xA7));
+	test('ResetBit4InB', () => runRegisterTests('b', 4, 0xA0));
+	test('ResetBit4InC', () => runRegisterTests('c', 4, 0xA1));
+	test('ResetBit4InD', () => runRegisterTests('d', 4, 0xA2));
+	test('ResetBit4InE', () => runRegisterTests('e', 4, 0xA3));
+	test('ResetBit4InH', () => runRegisterTests('h', 4, 0xA4));
+	test('ResetBit4InL', () => runRegisterTests('l', 4, 0xA5));
+
+	test('ResetBit5InA', () => runRegisterTests('a', 5, 0xAF));
+	test('ResetBit5InB', () => runRegisterTests('b', 5, 0xA8));
+	test('ResetBit5InC', () => runRegisterTests('c', 5, 0xA9));
+	test('ResetBit5InD', () => runRegisterTests('d', 5, 0xAA));
+	test('ResetBit5InE', () => runRegisterTests('e', 5, 0xAB));
+	test('ResetBit5InH', () => runRegisterTests('h', 5, 0xAC));
+	test('ResetBit5InL', () => runRegisterTests('l', 5, 0xAD));
+
+	test('ResetBit6InA', () => runRegisterTests('a', 6, 0xB7));
+	test('ResetBit6InB', () => runRegisterTests('b', 6, 0xB0));
+	test('ResetBit6InC', () => runRegisterTests('c', 6, 0xB1));
+	test('ResetBit6InD', () => runRegisterTests('d', 6, 0xB2));
+	test('ResetBit6InE', () => runRegisterTests('e', 6, 0xB3));
+	test('ResetBit6InH', () => runRegisterTests('h', 6, 0xB4));
+	test('ResetBit6InL', () => runRegisterTests('l', 6, 0xB5));
+
+	test('ResetBit7InA', () => runRegisterTests('a', 7, 0xBF));
+	test('ResetBit7InB', () => runRegisterTests('b', 7, 0xB8));
+	test('ResetBit7InC', () => runRegisterTests('c', 7, 0xB9));
+	test('ResetBit7InD', () => runRegisterTests('d', 7, 0xBA));
+	test('ResetBit7InE', () => runRegisterTests('e', 7, 0xBB));
+	test('ResetBit7InH', () => runRegisterTests('h', 7, 0xBC));
+	test('ResetBit7InL', () => runRegisterTests('l', 7, 0xBD));
 	// endregion
 });
--- src/Emulator/CPU/InstructionSet/Operators/BitManipulation/Reset.ts
+++ src/Emulator/CPU/InstructionSet/Operators/BitManipulation/Reset.ts
@@ -1,5 +1,78 @@
-import {OperatorInterface} from '../../InstructionManager';
+import {HardwareBusInterface} from '../../../../Hardware';
+import {RegisterKey} from '../../../Registers';
+import {Operator, OperatorInterface} from '../../InstructionManager';
+
+const resetRegister = (key: RegisterKey, position: number, hardware: HardwareBusInterface): void => {
+	const registers = hardware.registers;
+
+	registers[key] &= ~(1 << position);
+	registers.m = 2;
+};
 
 export const ResetOperators: OperatorInterface[] = [
+	// region Registers
+	new Operator('ResetBit0InA', 0x87, hardware => resetRegister('a', 0, hardware)),
+	new Operator('ResetBit0InB', 0x80, hardware => resetRegister('b', 0, hardware)),
+	new Operator('ResetBit0InC', 0x81, hardware => resetRegister('c', 0, hardware)),
+	new Operator('ResetBit0InD', 0x82, hardware => resetRegister('d', 0, hardware)),
+	new Operator('ResetBit0InE', 0x83, hardware => resetRegister('e', 0, hardware)),
+	new Operator('ResetBit0InH', 0x84, hardware => resetRegister('h', 0, hardware)),
+	new Operator('ResetBit0InL', 0x85, hardware => resetRegister('l', 0, hardware)),
 
+	new Operator('ResetBit1InA', 0x8F, hardware => resetRegister('a', 1, hardware)),
+	new Operator('ResetBit1InB', 0x88, hardware => resetRegister('b', 1, hardware)),
+	new Operator('ResetBit1InC', 0x89, hardware => resetRegister('c', 1, hardware)),
+	new Operator('ResetBit1InD', 0x8A, hardware => resetRegister('d', 1, hardware)),
+	new Operator('ResetBit1InE', 0x8B, hardware => resetRegister('e', 1, hardware)),
+	new Operator('ResetBit1InH', 0x8C, hardware => resetRegister('h', 1, hardware)),
+	new Operator('ResetBit1InL', 0x8D, hardware => resetRegister('l', 1, hardware)),
+
+	new Operator('ResetBit2InA', 0x97, hardware => resetRegister('a', 2, hardware)),
+	new Operator('ResetBit2InB', 0x90, hardware => resetRegister('b', 2, hardware)),
+	new Operator('ResetBit2InC', 0x91, hardware => resetRegister('c', 2, hardware)),
+	new Operator('ResetBit2InD', 0x92, hardware => resetRegister('d', 2, hardware)),
+	new Operator('ResetBit2InE', 0x93, hardware => resetRegister('e', 2, hardware)),
+	new Operator('ResetBit2InH', 0x94, hardware => resetRegister('h', 2, hardware)),
+	new Operator('ResetBit2InL', 0x95, hardware => resetRegister('l', 2, hardware)),
+
+	new Operator('ResetBit3InA', 0x9F, hardware => resetRegister('a', 3, hardware)),
+	new Operator('ResetBit3InB', 0x98, hardware => resetRegister('b', 3, hardware)),
+	new Operator('ResetBit3InC', 0x99, hardware => resetRegister('c', 3, hardware)),
+	new Operator('ResetBit3InD', 0x9A, hardware => resetRegister('d', 3, hardware)),
+	new Operator('ResetBit3InE', 0x9B, hardware => resetRegister('e', 3, hardware)),
+	new Operator('ResetBit3InH', 0x9C, hardware => resetRegister('h', 3, hardware)),
+	new Operator('ResetBit3InL', 0x9D, hardware => resetRegister('l', 3, hardware)),
+
+	new Operator('ResetBit4InA', 0xA7, hardware => resetRegister('a', 4, hardware)),
+	new Operator('ResetBit4InB', 0xA0, hardware => resetRegister('b', 4, hardware)),
+	new Operator('ResetBit4InC', 0xA1, hardware => resetRegister('c', 4, hardware)),
+	new Operator('ResetBit4InD', 0xA2, hardware => resetRegister('d', 4, hardware)),
+	new Operator('ResetBit4InE', 0xA3, hardware => resetRegister('e', 4, hardware)),
+	new Operator('ResetBit4InH', 0xA4, hardware => resetRegister('h', 4, hardware)),
+	new Operator('ResetBit4InL', 0xA5, hardware => resetRegister('l', 4, hardware)),
+
+	new Operator('ResetBit5InA', 0xAF, hardware => resetRegister('a', 5, hardware)),
+	new Operator('ResetBit5InB', 0xA8, hardware => resetRegister('b', 5, hardware)),
+	new Operator('ResetBit5InC', 0xA9, hardware => resetRegister('c', 5, hardware)),
+	new Operator('ResetBit5InD', 0xAA, hardware => resetRegister('d', 5, hardware)),
+	new Operator('ResetBit5InE', 0xAB, hardware => resetRegister('e', 5, hardware)),
+	new Operator('ResetBit5InH', 0xAC, hardware => resetRegister('h', 5, hardware)),
+	new Operator('ResetBit5InL', 0xAD, hardware => resetRegister('l', 5, hardware)),
+
+	new Operator('ResetBit6InA', 0xB7, hardware => resetRegister('a', 6, hardware)),
+	new Operator('ResetBit6InB', 0xB0, hardware => resetRegister('b', 6, hardware)),
+	new Operator('ResetBit6InC', 0xB1, hardware => resetRegister('c', 6, hardware)),
+	new Operator('ResetBit6InD', 0xB2, hardware => resetRegister('d', 6, hardware)),
+	new Operator('ResetBit6InE', 0xB3, hardware => resetRegister('e', 6, hardware)),
+	new Operator('ResetBit6InH', 0xB4, hardware => resetRegister('h', 6, hardware)),
+	new Operator('ResetBit6InL', 0xB5, hardware => resetRegister('l', 6, hardware)),
+
+	new Operator('ResetBit7InA', 0xBF, hardware => resetRegister('a', 7, hardware)),
+	new Operator('ResetBit7InB', 0xB8, hardware => resetRegister('b', 7, hardware)),
+	new Operator('ResetBit7InC', 0xB9, hardware => resetRegister('c', 7, hardware)),
+	new Operator('ResetBit7InD', 0xBA, hardware => resetRegister('d', 7, hardware)),
+	new Operator('ResetBit7InE', 0xBB, hardware => resetRegister('e', 7, hardware)),
+	new Operator('ResetBit7InH', 0xBC, hardware => resetRegister('h', 7, hardware)),
+	new Operator('ResetBit7InL', 0xBD, hardware => resetRegister('l', 7, hardware)),
+	// endregion
 ];
